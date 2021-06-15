ff2c33483cf3d9465b61dfe55855001cf8ed0792
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-06-15 14:57:01 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,56 @@
-var swiper = new Swiper('.swiper-container', {
-    autoplay: true,
-    autoplayDisableOnInteraction: false,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
+/*DESARROLLADO POR FERNANDO JUAREZ ALDORICA*/
+//CLASE ACTIVO
+let enlace = document.querySelectorAll('.lista li a');
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+enlace.forEach((element) => {
+	element.addEventListener('click', (event) => {
+		enlace.forEach((link) => {
+			link.classList.remove('activo');
+		});
+		event.target.classList.add('activo');
+	});
+});
+
+//HOME
+const btnMenu = document.querySelector('.btnMenu');
+let on_off = true;
+
+window.onscroll = () => {
+    //HOME
+    if (window.pageYOffset >= 500) {
+    	document.querySelector('.home').style.right = '20px';
+    }else{
+    	document.querySelector('.home').style.right = '-100%';
+    }
+
+    document.querySelector('.home').addEventListener('click', () => {
+    	document.body.scrollTop = '0';
+    	document.documentElement.scrollTop = '0';
+    });
+}
+
+
+let menu = document.querySelector('.contenedorMenu');
+let containerMenu = document.querySelector('.menu');
+let activador = true;
+
+
+//Menu de animacion
+btnMenu.addEventListener('click', () =>{
+    
+    document.querySelector('.btnMenu i').classList.toggle('fa-times');
+
+    if(activador){
+        menu.style.left = '0';
+        menu.style.transition = '0.5s';
+
+        activador = false;
+    }else{
+        activador = false;
+
+        menu.style.left = '-100%';
+        menu.style.transition = '0.5s';
+
+        activador = true;
+    }
+});
