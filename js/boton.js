5d7b718b6c192b75d3b1f912fc674d9ce8de155f
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-08-09 15:12:22 -0500

    Add files via upload

--- js/boton.js
+++ js/boton.js
@@ -1,3 +1,3 @@
-function alert() {
-  alert("Funcion en estado Beta, vuelve pronto");
+document.getElementById('alert').onclick = function(){
+    alert('Funcion en estado Beta, vuelve más tarde');
 }
